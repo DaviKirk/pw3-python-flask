a11ca4f2b53cecc04f76ea65219993a9de8d538c
DaviKirk <davikirk2008@gmail.com>
2025-02-21 11:52:07 -0300

    @

--- projeto-the-games/app.py
+++ projeto-the-games/app.py
@@ -1,20 +1,32 @@
 # pip gerenciador de pacotes do python
 # pip install flask
 # importando pacote do Flask
-from flask import Flask
+from flask import Flask, render_template
 
 # carregando o Flask na variável app
-app = Flask(__name__)
+app = Flask(__name__, template_folder="views")
 
 # criando a rota principal do site
 @app.route('/')
 # criando função no python
+#view function - Função de visualização
 def home():
-    return '<h1>Meu primeiro site em Flask, seja bem-vindo!</h1>'
+   
+    return render_template('index.html',)
 
 @app.route('/games')
 def games():
-    return '<h1>Seja Bem vindo a pagina de games</h1>'
+    titulo = 'CS-GO'
+    ano = 2012
+    categoria = 'FPS Online'
+    jogadores = ["Mc Rodolfinho", "davi_lambari", "juju_do_pix", "suaIrmã", "edsonGf"]
+    games = ['My Summer Car', 'Assetto Corsa', 'Beamg Drive', 'Forza Horizon 4', 'Minecraft', 'Grand Theft Auto V', 'Ragnarök']
+    return render_template('games.html',
+                           titulo=titulo,
+                           ano=ano,
+                           categoria=categoria,
+                           jogadores=jogadores,
+                           games=games)
 
 if __name__ == '__main__':
     # Rodando o servidor no localhost, porta 5000
