df9699577355f527c10de88d5a774af40dd44714
DaviKirk <davikirk2008@gmail.com>
2025-02-28 11:10:42 -0300

    @DaviKirk

--- projeto-the-games/app.py
+++ projeto-the-games/app.py
@@ -7,26 +7,31 @@ from flask import Flask, render_template
 app = Flask(__name__, template_folder="views")
 
 # criando a rota principal do site
+
+
 @app.route('/')
 # criando função no python
-#view function - Função de visualização
+# view function - Função de visualização
 def home():
-   
+
     return render_template('index.html',)
+
 
 @app.route('/games')
 def games():
-    titulo = 'CS-GO'
-    ano = 2012
-    categoria = 'FPS Online'
-    jogadores = ["Mc Rodolfinho", "davi_lambari", "juju_do_pix", "suaIrmã", "edsonGf"]
-    games = ['My Summer Car', 'Assetto Corsa', 'Beamg Drive', 'Forza Horizon 4', 'Minecraft', 'Grand Theft Auto V', 'Ragnarök']
+
+    game = {'Título': 'My summer car',
+            'Ano': 2016,
+            'categoria': 'Mundo Aberto'}
+
+    jogadores = ["Mc Rodolfinho", "davi_lambari",
+                 "juju_do_pix", "suaIrmã", "edsonGf"]
+
+
     return render_template('games.html',
-                           titulo=titulo,
-                           ano=ano,
-                           categoria=categoria,
-                           jogadores=jogadores,
-                           games=games)
+                       game=game,
+                       jogadores=jogadores
+                       )
 
 if __name__ == '__main__':
     # Rodando o servidor no localhost, porta 5000
