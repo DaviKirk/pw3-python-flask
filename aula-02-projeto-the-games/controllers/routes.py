f572873ef5c844de4cd7f122f6b86b744efab85e
DaviKirk <davikirk2008@gmail.com>
2025-02-28 12:28:39 -0300

    @DaviKirk

--- aula-02-projeto-the-games/controllers/routes.py
+++ aula-02-projeto-the-games/controllers/routes.py
@@ -1,10 +1,12 @@
-from flask import render_template
+from flask import render_template, request
+
+jogadores = ["Mc Rodolfinho", "davi_lambari",
+             "juju_do_pix", "suaIrmã", "edsonGf"]
 
 
 def init_app(app):
 
     # criando a rota principal do site
-
 
     @app.route('/')
     # criando função no python
@@ -13,18 +15,18 @@ def init_app(app):
 
         return render_template('index.html',)
 
-
-    @app.route('/games')
+    @app.route('/games', methods=['GET', 'POST'])
     def games():
 
         game = {'Título': 'My summer car',
                 'Ano': 2016,
                 'categoria': 'Mundo Aberto'}
 
-        jogadores = ["Mc Rodolfinho", "davi_lambari",
-                    "juju_do_pix", "suaIrmã", "edsonGf"]
+        if request.method == 'POST':
+            if request.form.get('jogador'):  # name do input
+                jogadores.append(request.form.get('jogador'))
 
         return render_template('games.html',
-                            game=game,
-                            jogadores=jogadores
-                            )
+                               game=game,
+                               jogadores=jogadores
+                               )
